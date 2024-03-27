c377b5f8b116757730ae5cbb70337696e77a2ae0
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-27 12:42:12 +0000

    Creating the dealer varaibles

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -8,6 +8,13 @@ public class BlackJack {
 
     // Class Variable
     private ArrayList<Card> deck;
+    private Random random = new Random();
+
+    // Dealer Variables
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum;
+    int dealerAceCount;
 
     // Constructor
     public BlackJack(){
@@ -16,11 +23,20 @@ public class BlackJack {
 
     // Method to start the game
     public void startGame(){
-        // deck
+        // Generate deck
         buildDeck();
+        // Shuffles the deck randomly
+        shuffleDeck();
+
+        // Dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount= 0;
+
 
     }
 
+    // Method that generates the deck of cards
     private void buildDeck() {
         deck = new ArrayList<Card>();
         String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -37,4 +53,21 @@ public class BlackJack {
         System.out.println("BUILD DECK: ");
         System.out.println(deck);
     }
+
+    private void shuffleDeck() {
+        for (int i = 0; i < deck.size(); i++) {
+            int j = random.nextInt(deck.size());
+            Card currentCard = deck.get(i);
+            Card randomCard = deck.get(j);
+            deck.set(i, randomCard);
+            deck.set(j, currentCard);
+        }
+        System.out.println("AFTER SHUFFLE: ");
+        System.out.println(deck);
+    }
+
+
+
+
+
 }
