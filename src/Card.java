3405d100d36a3da3d00083d35f8c9bca2bedebcc
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-27 12:34:38 +0000

    Creating the Card class and generating the deck

---
+++ src/Card.java
@@ -0,0 +1,16 @@
+public class Card {
+
+    // Variables
+    String value;
+    String type;
+
+
+    // Constructor
+    public Card(String value, String type){
+        this.value = value;
+        this.type = type;
+    }
+
+    // toString method
+    public String to
+}
